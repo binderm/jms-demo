7b25f432c8113d4bfd4b646a8d446a8021e6bb79
mrclbndr <marcel.binder@mailbox.org>
2017-06-09 14:32:27 +0200

    removed escalation from create script

--- docker/create-docker-images.sh
+++ docker/create-docker-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker build -t jms-demo-master -f master/Dockerfile master/
-sudo docker build -t jms-demo-slave -f slave/Dockerfile slave/
+docker build -t jms-demo-master -f master/Dockerfile master/
+docker build -t jms-demo-slave -f slave/Dockerfile slave/
 
